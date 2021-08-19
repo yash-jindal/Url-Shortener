54a82c220c3da8972215d631c53c25fec47098fc
Yash Jindal <yjindal87@gmail.com>
2021-08-19 15:36:00 +0530

    First commit

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const shortId = require('shortid');
 const bodyParser = require('body-parser');
+require('dotenv').config()
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://yash:user-is-best@cluster0.svleh.mongodb.net/UrlDB", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://yash:"+process.env.PASSWORD+"@cluster0.svleh.mongodb.net/UrlDB", { useNewUrlParser: true, useUnifiedTopology: true});
 
 const shortUrlSchema = new mongoose.Schema ({
 full: {
